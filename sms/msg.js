25efd99247a5f15e19e9cf0f4ac3cc47a10e6955
luffyelbrabo <106891190+luffyelbrabo@users.noreply.github.com>
2023-12-15 00:59:37 -0300

    Create msg.js

---
+++ sms/msg.js
@@ -0,0 +1,1 @@
+{"SendMessage":"1","MyMessage":"Olá tudo bem!, Não esqueça de fazer recarga mínima no seu chip de no máximo de 2 em 2 meses para evitar o bloqueio do seu chip!!  Em caso de sinal bloqueado não irá pega nossa internet dúvidas procure seu revendedor."}
